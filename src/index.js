2faeebce7386b82232b10af5c59b7aa8f6579b08
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2023-08-03 10:05:26 +0000

    Added the addjob and addprocess route and feature

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-// import {sequelize} from "./../db/database.mjs";
 const sequelize = require("./../db/database");
-// import fs from "fs";
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 const User = require("./../model/User");
+const Job = require("./../model/Job");
+const Process = require("./../model/Process");
 
 dotenv.config();
 
 const app = express();
 const PORT_NUMBER = Number(process.env.PROCESS) || 3000;
-const TOKEN_KEY = process.env.SECRET;
+const TOKEN_KEY = process.env.SECRET || "kay";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -45,51 +45,67 @@ app.get("/", (req, res) => {
 
 //post requests
 app.post("/login", async (req, res) => {
-  //   let loginData = req.body;
-  //   console.log(loginData);
-  //   try {
-  //     let movies = JSON.parse(fs.readFileSync(__dirname + "/data.json", "utf8"));
-  //     // Create token
-  //     console.log(TOKEN_KEY);
-  //     const token = jwt.sign(loginData, TOKEN_KEY, {
-  //       expiresIn: "1h",
-  //     });
-  //     const DATA_BASE = new DB();
-  //     DATA_BASE.verify(loginData.email, sort);
-  //     //callback function
-  //     function sort(row) {
-  //       if (row) {
-  //         // verifying password
-  //         bcrypt.compare(
-  //           loginData.password,
-  //           row.user_password,
-  //           function (err, result) {
-  //             let finalMovie;
-  //             if (result) {
-  //               finalMovie = addBookmark(row, movies);
-  //               console.log("token\n" + token);
-  //             }
-  //             finalMovie = finalMovie ? finalMovie : [];
-  //             res.json({ result, finalMovie, token });
-  //           }
-  //         );
-  //       } else {
-  //         res.json("login failed");
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     res.json({ error });
-  //   }
-});
-app.post("/user", (req, res) => {
-  console.log(req.body)
-  let data={
-
+  let loginData = req.body;
+  try {
+    // Create token
+    console.log(TOKEN_KEY);
+    const token = jwt.sign(loginData, TOKEN_KEY, {
+      expiresIn: "1h",
+    });
+    const SALTROUNDS = 10;
+    bcrypt.genSalt(SALTROUNDS, function (err, salt) {
+      bcrypt.hash(req.body.password, salt, function (err, hash) {
+        User.findOne({
+          where: {
+            email: req.body.email,
+          },
+        }).then((response) => {
+          if (response) {
+            bcrypt.compare(
+              req.body.password,
+              response.password,
+              (err, isMatch) => {
+                if (isMatch) {
+                  res.send({ ...response, token });
+                }
+              }
+            );
+          }
+        });
+        if (err) {
+          res.send("anfa");
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
   }
-  User.create(req.body).then(()=>{
-    res.send('success')
-  })
+});
+app.post("/adduser", (req, res) => {
+  console.log(req.body);
+  const SALTROUNDS = 10;
+  bcrypt.genSalt(SALTROUNDS, function (err, salt) {
+    bcrypt.hash(req.body.password, salt, function (err, hash) {
+      req.body.password = hash;
+      User.create(req.body).then(() => {
+        res.send("success");
+      });
+      if (err) {
+        res.send("anfa");
+      }
+    });
+  });
+});
+app.get("/user", (req, res) => {
+  console.log(req.body);
+  User.findAll({
+    where: {
+      id: req.body.id,
+    },
+  }).then((output) => {
+    res.send(output);
+  });
 });
 
 app.post("/signup", async (req, res) => {
@@ -101,18 +117,46 @@ app.post("/signup", async (req, res) => {
   //   }
   //   const SALTROUNDS = 10;
   //   let loginData = req.body;
-  //   // hashing password
-  //   bcrypt.genSalt(SALTROUNDS, function (err, salt) {
-  //     bcrypt.hash(loginData.password, salt, function (err, hash) {
-  //       const DATA_BASE = new DB();
-  //       DATA_BASE.create(loginData.email, hash, resSender);
-  //       console.log(hash);
-  //     });
+  // hashing password
+  // bcrypt.genSalt(SALTROUNDS, function (err, salt) {
+  //   bcrypt.hash(loginData.password, salt, function (err, hash) {
+  //     const DATA_BASE = new DB();
+  //     DATA_BASE.create(loginData.email, hash, resSender);
+  //     console.log(hash);
   //   });
+  // });
   // } catch (error) {
   //   console.log(error);
   //   res.json({ error });
   // }
+});
+
+app.post("/addjob", (req, res) => {
+  console.log(req.body);
+  let input = { ...req.body };
+  Job.create(input)
+    .then((jobOutput) => {
+      Process.create({ ...input, jobId: jobOutput.id })
+        .then(() => {
+          res.send("added");
+        })
+        .catch(() => {
+          res.send("unable to create process");
+        });
+    })
+    .catch(() => {
+      res.send("unable to create job");
+    });
+});
+
+app.post("/addprocess", (req, res) => {
+  Process.create(req.body)
+    .then(() => {
+      res.send("added");
+    })
+    .catch(() => {
+      res.send("unable to create process");
+    });
 });
 
 app.listen(PORT_NUMBER, () => {
